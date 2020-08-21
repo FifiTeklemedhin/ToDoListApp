8d48d47b6bc78ff2c6eef730ec8efd0743ddb691
FifiTeklemedhin <fi.leul3562@gmail.com>
2020-08-21 10:45:20 -0600

    commented out all implemented functions, added more nodes to the heap.

--- MaxHeapLinkedList.py
+++ MaxHeapLinkedList.py
@@ -10,17 +10,19 @@ class MaxHeap:
     # public methods
 
     def push(self, data):
-        #inserts a value in the LinkedList as the new tail, then heapifies up to restore the heap
+        # Inserts a value in the LinkedList as the new tail, then heapifies up to restore the heap
         self.size += 1
         self.heap.insert_val(self.size, data)
         self.__heapify_up(self.size)
 
     def peek(self):
+        # Returns the root without deleting it
         if self.size <= 0:
             return
         return self.heap.find(0)
 
     def pop(self):
+        # Deletes the root, and returns its value
         if self.size <= 0:
             return
 
@@ -30,20 +32,23 @@ class MaxHeap:
         self.__heapify_down()
 
         return root
-    # private methods
+    # Private methods
 
     def __heapify_up(self, index):
+        # Recursively bubbles up from the last (and newest) index, restoring the heap
         parent = index // 2
+
+        # Means you're already at the top of the heap
         if index <= 1:
-            print("index is root")
             return
 
+        # Finds out the rank of the parent and the index
         index_priority = self.heap.find(index).get_data()[0]
         parent_priority = self.heap.find(parent).get_data()[0]
 
-        print("parent: {}\n index: {}".format(parent_priority, index_priority))
+        # Checks to see if the index value is higher than its parent. If so, it swaps their information so that the
+        # highest priority is on top. It then continues to heapify from that highest location
         if parent_priority < index_priority:
-            print("swapping {} and {}".format(index_priority, parent_priority))
             self.__swap(parent, index)
             self.__heapify_up(index)
 
@@ -51,7 +56,7 @@ class MaxHeap:
     def __heapify_down(self, index):
         pass
     def __swap(self, pos_1, pos_2):
-        # swaps the data of two indexes, so that the node higher up is the one with a higher priority
+        # Swaps the data of two indexes, so that the node higher up is the one with a higher priority.
         node_1 = self.heap.find(pos_1)
         node_2 = self.heap.find(pos_2)
         node_1_holder = node_1.get_data()
@@ -60,6 +65,10 @@ class MaxHeap:
         node_2.set_data(node_1_holder)
 
     def __repr__(self):
+        # The method iterates through all of the levels of the tree in the first loop.
+        # The indices on that level are traversed in the nested loop: 2 ^ level - 1 through 2 ^ level.
+        # Since the traversal range does not account for whether the last level is actually full or not,
+        # There is a conditional that breaks out of the loop as soon as there is no Node at the given index.
         levels = self.num_levels()
         level_str = ""
         for i in range(1, levels):
@@ -72,7 +81,13 @@ class MaxHeap:
         return level_str
 
     def num_levels(self, levels = 0):
-        if int(math.pow(2, levels)) >= self.size:
+        # 2 ^ level is the maximum amount of nodes the heap can have with that many levels. The levels start at 0
+        # because 2 ^ 0 is the number of nodes at the root The maximum amount of nodes with that many levels should
+        # be less than or equal to the size of the heap. If it is, then it is assumed that the level is full,
+        # and the next recursive call checks if the next level is full. If it is greater than the size,
+        # then the method returns the level + 1 because it started at 0. Otherwise, it continues.
+
+        if int(math.pow(2, levels)) > self.size:
             return levels + 1
 
         levels += 1
@@ -100,5 +115,10 @@ heap.push([3, "cooking"])
 heap.push([2, "paint nails"])
 heap.push([7, "email"])
 heap.push([1, "clubs"])
+heap.push([4, "reading"])
+heap.push([12, "studing"])
+heap.push([8, "night routine"])
+heap.push([15, "speech"])
+heap.push([0, "sleep"])
 print(heap.heap)
 print(heap)
